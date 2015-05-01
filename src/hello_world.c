827a7fa9fb9b47b8d206650866a27cf25eee9fab
Terence Lee <hone02@gmail.com>
2015-04-30 22:13:27 -0500

    return proper exit codes based on the mruby code

--- src/hello_world.c
+++ src/hello_world.c
@@ -13,6 +13,7 @@ int main(int argc, char *argv[])
   mrb_value v;
   mrb_value ARGV;
   int i;
+  int return_value = EXIT_SUCCESS;
 
   ARGV = mrb_ary_new_capa(mrb, argc);
   for (i = 0; i < argc; i++) {
@@ -21,8 +22,16 @@ int main(int argc, char *argv[])
   mrb_define_global_const(mrb, "ARGV", ARGV);
 
   c = mrbc_context_new(mrb);
-  mrb_load_irep_cxt(mrb, hello_world, c);
+  v = mrb_load_irep_cxt(mrb, hello_world, c);
   mrbc_context_free(mrb, c);
+
+  if (mrb->exc) {
+    if (!mrb_undef_p(v)) {
+      mrb_print_error(mrb);
+    }
+    return_value = EXIT_FAILURE;
+  }
   mrb_close(mrb);
-  return 0;
+
+  return return_value;
 }
