343b6f9118e5eca9e6604df7dc91f342a343df51
Terence Lee <hone02@gmail.com>
2015-04-30 19:45:48 -0500

    support cli arguments

--- mrblib/hello_world.rb
+++ mrblib/hello_world.rb
@@ -1,1 +1,6 @@
-puts 'hello world'
+if ARGV.size > 1
+  arguments = ARGV[1..-1]
+  puts "hello #{arguments.join(" ")}"
+else
+  puts "hello world"
+end
--- src/hello_world.c
+++ src/hello_world.c
@@ -4,12 +4,21 @@
 /* Include the mruby header */
 #include <mruby.h>
 #include <mruby/irep.h>
+#include <mruby/array.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
   mrb_state *mrb = mrb_open();
   mrbc_context *c;
   mrb_value v;
+  mrb_value ARGV;
+  int i;
+
+  ARGV = mrb_ary_new_capa(mrb, argc);
+  for (i = 0; i < argc; i++) {
+    mrb_ary_push(mrb, ARGV, mrb_str_new_cstr(mrb, argv[i]));
+  }
+  mrb_define_global_const(mrb, "ARGV", ARGV);
 
   c = mrbc_context_new(mrb);
   mrb_load_irep_cxt(mrb, hello_world, c);
