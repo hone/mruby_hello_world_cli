96e7b42d5648bfe9bed2bc9d43af4e414086ac04
Terence Lee <hone02@gmail.com>
2015-06-21 17:54:29 -0400

    setup cross compiling builds for osx + windows

--- build_config.rb
+++ build_config.rb
@@ -1,14 +1,38 @@
-MRuby::Build.new do |conf|
-  # Gets set by the VS command prompts.
-  if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
-    toolchain :visualcpp
-  else
-    toolchain :gcc
-  end
-
+def gem_config(conf)
   # conf.gembox 'default'
   conf.gem :core => 'mruby-print'
 
   # be sure to include this gem
   conf.gem File.expand_path(File.dirname(__FILE__))
+end
+
+MRuby::Build.new do |conf|
+  toolchain :gcc
+
+  gem_config(conf)
+end
+
+MRuby::CrossBuild.new('x86_64-apple-darwin14') do |conf|
+  toolchain :clang
+
+  [conf.cc, conf.linker].each do |cc|
+    cc.command = 'x86_64-apple-darwin14-clang'
+  end
+  conf.cxx.command      = 'x86_64-apple-darwin14-clang++'
+  conf.archiver.command = 'x86_64-apple-darwin14-ar'
+
+  gem_config(conf)
+end
+
+MRuby::CrossBuild.new('mingw-w64') do |conf|
+  toolchain :gcc
+
+  [conf.cc, conf.linker].each do |cc|
+    cc.command = 'x86_64-w64-mingw32-gcc'
+  end
+  conf.cxx.command      = 'x86_64-w64-mingw32-cpp'
+  conf.archiver.command = 'x86_64-w64-mingw32-gcc-ar'
+  conf.exts.executable  = ".exe"
+
+  gem_config(conf)
 end
